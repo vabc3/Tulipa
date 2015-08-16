b4b61eb00ce9b846020c032315424785223a4795
Karata@w8 <vabc3h@outlook.com>
2015-08-16 22:50:11 +0800

    maybe redu

--- Tulipe.WXService/HomeApiController.cs
+++ Tulipe.WXService/HomeApiController.cs
@@ -1,38 +1,36 @@
 ﻿using Senparc.Weixin.MP;
 using Senparc.Weixin.MP.Entities.Request;
 using Senparc.Weixin.MP.MvcExtension;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace Tulipe.WXService
 {
-    public class WeixinController : Controller
+    /*
+    public class WeixinController : ApiController
     {
         private const string Token = "weixin";//对应微信后台设置的Token，建议设置地复杂一些
 
         /// <summary>
         /// 微信后台验证地址（使用Get），微信后台的“接口配置信息”的Url填写如：http://weixin.senparc.com/weixin
         /// </summary>
-        [HttpGet]
-        [ActionName("Index")]
-        public ActionResult Get(PostModel postModel, string echostr)
+        public IHttpActionResult Get(PostModel postModel, string echostr)
         {
             if (CheckSignature.Check(postModel.Signature, postModel.Timestamp, postModel.Nonce, Token))
             {
-                return Content(echostr);//返回随机字符串则表示验证通过
+                return Ok(echostr);//返回随机字符串则表示验证通过
             }
             else
             {
-                return Content("failed:" + postModel.Signature + "," + CheckSignature.GetSignature(postModel.Timestamp, postModel.Nonce, Token) + "。如果你在浏览器中看到这句话，说明此地址可以被作为微信公众账号后台的Url，请注意保持Token一致。");
+                return Ok("failed:" + postModel.Signature + "," + CheckSignature.GetSignature(postModel.Timestamp, postModel.Nonce, Token) + "。如果你在浏览器中看到这句话，说明此地址可以被作为微信公众账号后台的Url，请注意保持Token一致。");
             }
         }
 
-        [HttpPost]
-        [ActionName("Index")]
-        public ActionResult Post(PostModel postModel)
+        public IHttpActionResult Post(PostModel postModel)
         {
             if (!CheckSignature.Check(postModel.Signature, postModel.Timestamp, postModel.Nonce, Token))
             {
-                return Content("参数错误！");
+                return Ok("参数错误！");
             }
 
             postModel.Token = Token;
@@ -46,4 +44,5 @@ namespace Tulipe.WXService
             return new WeixinResult(messageHandler);//返回结果
         }
     }
+    */
 }
